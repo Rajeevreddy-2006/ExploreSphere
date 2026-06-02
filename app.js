05aa87bd47c2c9a4f5805ab7ebc72803998ad943
Rajeev <jrajeevreddy2006@gmail.com>
2026-06-02 21:56:35 +0530

    New message

--- app.js
+++ app.js
@@ -23,8 +23,8 @@ const userRoutes = require('./routes/users');
 const campgroundRoutes = require('./routes/campgrounds');
 const reviewRoutes = require('./routes/reviews');
 
-const dbUrl = 'mongodb://localhost:27017/yelp-camp';
-//const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp';
+// const dbUrl = 'mongodb://localhost:27017/yelp-camp';
+const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp';
 //'mongodb://localhost:27017/yelp-camp'
 // process.env.DB_URL
 mongoose.connect(dbUrl)
